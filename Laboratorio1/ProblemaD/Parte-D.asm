0a46613ba35f2d018f6c6d87963cbfcb60bd738d
JacintoCarbone <jacinto.carbone@estudiantes.utec.edu.uy>
2026-09-01 10:11:11 -0300

    Jacinto resuelve

--- Laboratorio1/ProblemaD/Parte-D.asm
+++ Laboratorio1/ProblemaD/Parte-D.asm
@@ -1,6 +1,5 @@
 .include "m328pdef.inc"
 
-    
     .def temp = r16
     .def A = r17
     .def B = r18
@@ -9,41 +8,41 @@
     .def C_flag = r21
     .def Z_flag = r22
     .def N_flag = r23
+
 ;Operador A
-    .equ A0 = PD0
-    .equ A1 = PD1
-    .equ A2 = PD2
-    .equ A3 = PD3
+    .equ A0 = PC4 ; Pin A4 (Bit 0)
+    .equ A1 = PC5 ; Pin A5 (Bit 1)
+    .equ A2 = PD2 ; Pin 2  (Bit 2)
+    .equ A3 = PD3 ; Pin 3  (Bit 3)
 
 ;Operador B
-    .equ B0 = PD4
-    .equ B1 = PD5
-    .equ B2 = PD6
-    .equ B3 = PD7 
+    .equ B0 = PD4 ; Pin 4  (Bit 0)
+    .equ B1 = PD5 ; Pin 5  (Bit 1)
+    .equ B2 = PD6 ; Pin 6  (Bit 2)
+    .equ B3 = PD7 ; Pin 7  (Bit 3)
 
 ;Selector
-    .equ S0 = PC0
-    .equ S1 = PC1
-    .equ S2 = PC2
+    .equ S0 = PC0 ; Pin A0
+    .equ S1 = PC1 ; Pin A1
+    .equ S2 = PC2 ; Pin A2
 
 ;Salidas
-    .equ F0 = PB0
-    .equ F1 = PB1
-    .equ F2 = PB2
-    .equ F3 = PB3
-    .equ OUT_C = PB4
-    .equ OUT_Z = PB5
-    .equ OUT_N = PC3
+    .equ F0 = PB0    ; Pin 8
+    .equ F1 = PB1    ; Pin 9
+    .equ F2 = PB2    ; Pin 10
+    .equ F3 = PB3    ; Pin 11
+    .equ OUT_C = PB4 ; Pin 12
+    .equ OUT_Z = PB5 ; Pin 13
+    .equ OUT_N = PC3 ; Pin A3
 
 .org 0x0000
     rjmp inicio 
 
 inicio:
-
     ldi temp, HIGH(RAMEND)
-    out  SPH, temp
-    ldi  temp, LOW(RAMEND)
-    out  SPL, temp
+    out SPH, temp
+    ldi temp, LOW(RAMEND)
+    out SPL, temp
 
 ;Puerto D como entradas
     ldi temp, 0x00
@@ -56,35 +55,43 @@ inicio:
     ldi temp, 0x00
     out PORTB, temp
 
-;Puerto C como salidas y entradas
+;Puerto C: PC3 salida (N), demás entradas
     ldi temp, 0b00001000
     out DDRC, temp
     ldi temp, 0x00
     out PORTC, temp
 
-
 main_loop: 
-;Lectura constante
-    in temp, PIND ;Lee los 8 pines de D (A y B juntos)
-    mov A, temp ;Guarda el valor en A
-    andi A, 0x0F ;Toma solo los 4 pines de A
-    mov B, temp ;Guarda el valor en B
-    andi B, 0xF0 ;Toma solo los 4 pines de B
-    swap B ;Invierte B para tenerlo en orden
-    in temp, PINC ;Lee los pines de C
-    mov S, temp ;Guarda el valor en S
-    andi S, 0x07 ;Toma solo los 3 pines de S
+    ; 1. Leer B (PD4..PD7) y A2, A3 (PD2, PD3)
+    in temp, PIND
+    mov B, temp
+    andi B, 0xF0
+    swap B              ; B listo (bits 0..3)
+
+    mov A, temp
+    andi A, 0x0C        ; A tiene listos los bits 2 y 3
+
+    ; 2. Leer Selector S (PC0..PC2) y A0, A1 (PC4, PC5)
+    in temp, PINC
+    mov S, temp
+    andi S, 0x07        ; S listo (bits 0..2)
+
+    sbrc temp, 4        ; Si A4 (PC4) está en 1 -> bit 0 de A = 1
+    ori A, 0x01
+    sbrc temp, 5        ; Si A5 (PC5) está en 1 -> bit 1 de A = 1
+    ori A, 0x02
+
     rcall selector
     
-    ;Salida a puertos
-    out PORTB, F ; F en PB0-PB3 (limpia PB4-PB5)
+    ; 3. Salida a puertos
+    out PORTB, F
     sbrc C_flag, 0
-    sbi PORTB, OUT_C ; C en PB4
+    sbi PORTB, OUT_C
     sbrc Z_flag, 0
-    sbi PORTB, OUT_Z ; Z en PB5
-    cbi PORTC, OUT_N ; Limpiar N primero
+    sbi PORTB, OUT_Z
+    cbi PORTC, OUT_N
     sbrc N_flag, 0
-    sbi PORTC, OUT_N ; N en PC3
+    sbi PORTC, OUT_N
 
     rjmp main_loop
 
@@ -108,46 +115,43 @@ selector:
     ret 
 
 op_clear: 
-    clr F ; Deja F en 0
-    clr C_flag ; No hay carry
+    clr F
+    clr C_flag
     rjmp end_operacion
 
 op_resta: ; A - B
     mov F, A
-    sub F, B ; F = A - B (8 bits), SREG.C = borrow
-
-    ; Leo el carry del SREG (LDI y BRCC no modifican SREG)
-    ldi C_flag, 0 ; Dejo el C_flag en 0
-    brcc resta_sin_carry ; Si C=0 (no hay borrow), salta de linea
-    ldi C_flag, 1       ; No hubo salto de linea entonces pongo C en 1
+    sub F, B
+    ldi C_flag, 0
+    brcc resta_sin_carry
+    ldi C_flag, 1
 resta_sin_carry:
     rjmp end_operacion
 
 op_suma: ; A + B
     mov F, A 
     add F, B
-    ; En suma de 4 bits, el carry queda en el bit 4 de F
-    ldi C_flag, 0 ; Dejo el C_flag en 0
-    sbrc F, 4 ; Si el bit 4 de F es 0, salta de linea
-    ldi C_flag, 1 ; No hubo salto de linea entonces pongo C en 1
+    ldi C_flag, 0
+    sbrc F, 4
+    ldi C_flag, 1
     rjmp end_operacion
 
 op_xor: ; A xor B
     mov F, A
     eor F, B
-    clr C_flag ; No hay carry en xor
+    clr C_flag
     rjmp end_operacion
 
 op_and: ; A and B
     mov F, A
     and F, B
-    clr C_flag ; No hay carry en and
+    clr C_flag
     rjmp end_operacion
 
 op_or: ; A or B
     mov F, A 
     or F, B
-    clr C_flag ; No hay carry en or
+    clr C_flag
     rjmp end_operacion
 
 op_shl: ; A << 1
@@ -166,21 +170,16 @@ op_inc: ; A + 1
     ldi C_flag, 1
     rjmp end_operacion
 
-
-; Calculo de Z, N y retorno
 end_operacion:
-    ; Dejo solo el resultado de 4 bits
-    andi F, 0x0F        ; ANDI pone Z=1 en SREG si F queda en 0
+    andi F, 0x0F
 
-    ; Z flag: basada en resultado de 4 bits
-    ldi Z_flag, 0 ; Dejo el Z_flag en 0
-    brne sin_zero  ; Si el SREG.Z es 1 (F != 0), salta de linea
-    ldi Z_flag, 1 ; No hubo salto de linea entonces pongo Z en 1
+    ldi Z_flag, 0
+    brne sin_zero
+    ldi Z_flag, 1
 sin_zero:
 
-    ; N flag: signo en bit 3
-    ldi N_flag, 0 ; Dejo el N_flag en 0
-    sbrc F, 3 ; Si bit 3 de F es 0, salta de linea
-    ldi N_flag, 1 ; No hubo salto de linea entonces pongo N en 1
+    ldi N_flag, 0
+    sbrc F, 3
+    ldi N_flag, 1
     
-    ret ; Vuelvo a main_loop
+    ret

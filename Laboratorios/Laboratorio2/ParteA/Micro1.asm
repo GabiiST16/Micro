24303958cc8ee874e179053e93f0ddf8d539eeb9
GabiiST16 <gabriel.gonzalez@estudiantes.utec.edu.uy>
2026-09-16 12:10:00 -0300

    Micro1: estructura inicial + config USART (TX a 9600 baud, 8N1)

--- Laboratorios/Laboratorio2/ParteA/Micro1.asm
+++ Laboratorios/Laboratorio2/ParteA/Micro1.asm
@@ -5,13 +5,14 @@
 .equ F_CPU = 16000000
 .equ bps = (F_CPU/(16*baud))-1
 
-; Pulsadores (Puerto D)
-.equ BTN0 = PD2       ; Bit 0
-.equ BTN1 = PD3       ; Bit 1
-.equ BTN2 = PD4       ; Bit 2
-.equ BTN_ENV = PD5    ; Botón de enviar
+; Pulsadores (Puerto B)
+.equ BTN0 = PB0       ; Pin 8  (Bit 0)
+.equ BTN1 = PB1       ; Pin 9  (Bit 1)
+.equ BTN2 = PB2       ; Pin 10 (Bit 2)
+.equ BTN_ENV = PB3    ; Pin 11 (Botón de enviar)
 
-.def temp = r16
+    .def temp = r16
+    .def dato = r17
 
 .org 0x0000
     rjmp inicio
@@ -22,18 +23,21 @@ inicio:
     ldi temp, LOW(RAMEND)
     out SPL, temp
 
-; Puerto D: PD1 (TX) como salida, PD2-PD5 como entradas
-    ldi temp, 0b00000010    ; Solo PD1 (TX) como salida
+; Puerto D: PD1 (TX) como salida
+    ldi temp, 0b00000010
     out DDRD, temp
-    ldi temp, 0b00111100    ; Pull-ups en PD2, PD3, PD4, PD5
-    out PORTD, temp
+
+; Puerto B como entradas (botones)
+    ldi temp, 0x00
+    out DDRB, temp
+    ldi temp, 0x0F          ; Pull-ups en PB0, PB1, PB2, PB3
+    out PORTB, temp
 
     rcall initUART
 
     rjmp main_loop
 
 main_loop:
-
     rjmp main_loop
 
 ; Inicialización de USART (solo TX)
@@ -41,25 +45,9 @@ initUART:
     ldi temp, HIGH(bps)
     sts UBRR0H, temp
     ldi temp, LOW(bps)
-    sts UBRR0L, temp          ; Corregido: era URR0L
+    sts UBRR0L, temp
     ldi temp, (1<<TXEN0)      ; Solo TX habilitado (MCU1 es transmisor)
     sts UCSR0B, temp
     ldi temp, (1<<UCSZ01)|(1<<UCSZ00)  ; 8 bits datos, 1 stop, sin paridad
     sts UCSR0C, temp
-    ret
-
-; Antirrebote (~20ms a 16MHz)
-delay_debounce:
-    ldi r18, 2
-deb_l1:
-    ldi r19, 210
-deb_l2:
-    ldi r20, 255
-deb_l3:
-    dec r20
-    brne deb_l3
-    dec r19
-    brne deb_l2
-    dec r18
-    brne deb_l1
     ret
